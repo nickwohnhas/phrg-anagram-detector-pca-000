aecf481ae682b9984d148d8d4fca4b6a6f9df537
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-23 09:50:00 -0400

    Built an Anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+ attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    empty_array = []
+    array.each do |anagram_word|
+     if anagram_word.split("").sort == @word.split("").sort
+      empty_array << anagram_word
+    end
+   end
+   empty_array
+  end
+
+end
